4d5c9013ecc345e01aec10f7924ebbba13a2ec56
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2024-10-30 16:17:00 +0700

    update: input verif (main.go)

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main(){
 	fmt.Printf("\n")
 
 	// Registration System
-	for{
+	for remainingTickets > 0{
 		fmt.Println("Get your tickets here!", "(Available:", conferenceTickets-soldTickets, "Tickets)")
 		fmt.Printf("\n")
 		fmt.Println("📄Registration Site!📄")
@@ -27,36 +27,54 @@ func main(){
 		var userTickets uint
 		fmt.Printf("Enter the number of tickets you want to purchase: ")
 		fmt.Scan(&userTickets) // Abs() for safety, fix later
+		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
 
-		if remainingTickets < userTickets{
+		if !isValidTickets{ 
 			fmt.Printf("Sorry, we only have %d tickets left!\n", remainingTickets)
 		} else{
+			temporaryIdentities := [][2]string{}
+			var isValidEmail, isValidName bool
 			for i := uint(1); i <= userTickets; i++ {
 				fmt.Printf("[%d]\n", i)
 				fmt.Printf("Enter your name\t: ")
 				fmt.Scan(&firstName)
+				
 				fmt.Printf("Enter your last name\t: ")
 				fmt.Scan(&lastName)
+				
 				fmt.Printf("Enter your email\t: ")
 				fmt.Scan(&email)
-				identities = append(identities, [2]string{firstName + " " + lastName, email})
+				
+				isValidName = len(firstName) > 2 && len(lastName) > 2
+				isValidEmail = strings.Contains(email, "@") && strings.Contains(email, ".")
+				
+				temporaryIdentities = append(temporaryIdentities, [2]string{firstName + " " + lastName, email})
 			}
-			soldTickets += userTickets
-			remainingTickets = conferenceTickets - soldTickets
-			
-			fmt.Printf("\n[%s ORDER DETAILS]\n", conferenceName)
-			fmt.Printf("Tickets\t: %d\n", userTickets)
-			for _, entry := range identities{
-				fmt.Printf("Name\t: %s\nEmail\t: %s\n", entry[0], entry[1])
+			if isValidName && isValidEmail{
+				for _, tempIdentity := range temporaryIdentities{
+					identities = append(identities, [2]string{tempIdentity[0], tempIdentity[1]})
+				}
+				soldTickets += userTickets
+				remainingTickets = conferenceTickets - soldTickets
+				
+				fmt.Printf("\n[%s ORDER DETAILS]\n", conferenceName)
+				fmt.Printf("Tickets\t: %d\n", userTickets)
+				for _, entry := range identities{
+					fmt.Printf("Name\t: %s\nEmail\t: %s\n", entry[0], entry[1])
+				}
+		
+				fmt.Printf("\n[ADMIN ONLY]")
+				firstNames := []string{}
+				for _, identity := range identities{
+					var names = strings.Fields(identity[0])
+					firstNames = append(firstNames, names[0])
+				}
+				fmt.Printf("The first names of the attendees are: %v\n", firstNames)
+			} else{
+				fmt.Printf("\n")
+				fmt.Println("⚠️Invalid name or email, please try again.⚠️")
+				fmt.Printf("\n")
 			}
-	
-			fmt.Printf("\n[ADMIN ONLY]")
-			firstNames := []string{}
-			for _, identity := range identities{
-				var names = strings.Fields(identity[0])
-				firstNames = append(firstNames, names[0])
-			}
-			fmt.Printf("The first names of the attendees are: %v\n", firstNames)
 		}
 
 		ticketsSoldOut := remainingTickets == 0
