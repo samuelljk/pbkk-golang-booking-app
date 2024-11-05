eb0a3dfef2a2562280bfc67b0e2f6110a30783e8
Samuel Josefano Kaloh <kalohsamuel@gmail.com>
2024-11-05 14:29:07 +0700

    update: reformatting with function (main.go)

--- main.go
+++ main.go
@@ -5,51 +5,60 @@ import (
 	"strings"
 )
 
-func main(){
+func welcomeMessage(){
 	var conferenceName = "Go-Conference"
+	fmt.Printf("\n")
+	fmt.Println("✨Welcome to", conferenceName, "application!✨")
+	fmt.Printf("\n")
+}
+
+func ticketsForm(conferenceTickets uint, soldTickets uint) uint{
+	fmt.Println("Get your tickets here!", "(Available:", conferenceTickets-soldTickets, "Tickets)")
+	fmt.Printf("\n")
+	fmt.Println("📄Registration Site!📄")
+	var userTickets uint
+	fmt.Printf("Enter the number of tickets you want to purchase: ")
+	fmt.Scan(&userTickets)
+	return userTickets
+}
+
+func userDataForm(userTickets uint) ([][2]string, bool, bool){
+	var identities = [][2]string{}
+	var isValidEmail, isValidName bool
+	for i := uint(1); i <= userTickets; i++ {
+		var firstName, lastName, email string
+		fmt.Printf("[%d]\n", i)
+		fmt.Printf("Enter your name\t: ")
+		fmt.Scan(&firstName)
+				
+		fmt.Printf("Enter your last name\t: ")
+		fmt.Scan(&lastName)
+				
+		fmt.Printf("Enter your email\t: ")
+		fmt.Scan(&email)
+		isValidName = len(firstName) > 2 && len(lastName) > 2
+		isValidEmail = strings.Contains(email, "@") && strings.Contains(email, ".")
+		identities = append(identities, [2]string{firstName + " " + lastName, email})
+	}
+	return identities, isValidName, isValidEmail
+}
+
+func main(){
 	const conferenceTickets = 5
 	var soldTickets uint = 0
 	var remainingTickets uint = conferenceTickets - soldTickets
-	
-	// Welcome Message
-	fmt.Printf("\n")
-	fmt.Println("✨Welcome to", conferenceName, "application!✨")
-
-	var identities [][2]string // [name, email]
-	fmt.Printf("\n")
-
-	// Registration System
+	var identities [][2]string // [name, email]	
+	welcomeMessage()
 	for remainingTickets > 0{
-		fmt.Println("Get your tickets here!", "(Available:", conferenceTickets-soldTickets, "Tickets)")
-		fmt.Printf("\n")
-		fmt.Println("📄Registration Site!📄")
-		var firstName, lastName, email string
-		var userTickets uint
-		fmt.Printf("Enter the number of tickets you want to purchase: ")
-		fmt.Scan(&userTickets) // Abs() for safety, fix later
+		// firstName, lastName, email
+		userTickets := ticketsForm(conferenceTickets, soldTickets)
 		isValidTickets := userTickets > 0 && userTickets <= remainingTickets
-
 		if !isValidTickets{ 
 			fmt.Printf("Sorry, we only have %d tickets left!\n", remainingTickets)
 		} else{
-			temporaryIdentities := [][2]string{}
+			var temporaryIdentities [][2]string
 			var isValidEmail, isValidName bool
-			for i := uint(1); i <= userTickets; i++ {
-				fmt.Printf("[%d]\n", i)
-				fmt.Printf("Enter your name\t: ")
-				fmt.Scan(&firstName)
-				
-				fmt.Printf("Enter your last name\t: ")
-				fmt.Scan(&lastName)
-				
-				fmt.Printf("Enter your email\t: ")
-				fmt.Scan(&email)
-				
-				isValidName = len(firstName) > 2 && len(lastName) > 2
-				isValidEmail = strings.Contains(email, "@") && strings.Contains(email, ".")
-				
-				temporaryIdentities = append(temporaryIdentities, [2]string{firstName + " " + lastName, email})
-			}
+			temporaryIdentities, isValidName, isValidEmail = userDataForm(userTickets)
 			if isValidName && isValidEmail{
 				for _, tempIdentity := range temporaryIdentities{
 					identities = append(identities, [2]string{tempIdentity[0], tempIdentity[1]})
@@ -57,7 +66,7 @@ func main(){
 				soldTickets += userTickets
 				remainingTickets = conferenceTickets - soldTickets
 				
-				fmt.Printf("\n[%s ORDER DETAILS]\n", conferenceName)
+				fmt.Printf("\n[ORDER DETAILS]\n")
 				fmt.Printf("Tickets\t: %d\n", userTickets)
 				for _, entry := range identities{
 					fmt.Printf("Name\t: %s\nEmail\t: %s\n", entry[0], entry[1])
